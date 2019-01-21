9b738b3197e79ab8eaf6b9e103bbe8ccbe82035b
loren.lugosch <loren.lugosch@gmail.com>
2019-01-21 11:23:42 -0500

    changes

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ with open("war_and_peace.txt", "r") as f:
 	lines = f.readlines()
 lines[-1] += '\n'
 
+# get size of input and output alphabets
 c = Counter(("".join(lines)))
 Sy = list(c.keys()) # set of possible output letters
 Sy_size = len(Sy) # 82, including EOS
@@ -27,18 +28,32 @@ total_lines = len(lines)
 
 # split dataset
 one_tenth = total_lines // 10
-train_dataset = lines[0:one_tenth * 8]
-valid_dataset = lines[one_tenth * 8: one_tenth * 9]
-test_dataset = lines[one_tenth * 9:]
+train_labels = lines[0:one_tenth * 8]
+train_input = ["".join(c for c in line if c not in "AEIOUaeiou") for line in train_labels]
+train_dataset = (train_input, train_labels)
 
+valid_labels = lines[one_tenth * 8: one_tenth * 9]
+valid_input = ["".join(c for c in line if c not in "AEIOUaeiou") for line in valid_labels]
+valid_dataset = (valid_input, valid_labels)
+
+test_labels = lines[one_tenth * 9:]
+test_input = ["".join(c for c in line if c not in "AEIOUaeiou") for line in test_labels]
+test_dataset = (test_input, test_labels)
+
+# initialize model
 model = EncoderDecoder(	num_encoder_layers=2,
 						num_encoder_hidden=128, 
 						num_decoder_layers=2, 
 						num_decoder_hidden=128, 
 						Sx_size=Sx_size, 
 						Sy_size=Sy_size,
-						y_eos=y_eos)
+						y_eos=y_eos,
+						dropout=True)
 
+num_epochs = 10
 for epoch in range(num_epochs):
 	train(model, train_dataset)
 	test(model, valid_dataset)
+
+test_input = "Hello, world!"
+# print(model.infer(test_input))
--- models.py
+++ models.py
@@ -2,16 +2,14 @@ import torch
 from helper_functions import one_hot
 
 class EncoderDecoder(torch.nn.Module):
-	def __init__(self, num_encoder_layers, num_encoder_hidden, num_decoder_layers, num_decoder_hidden, Sx_size, Sy_size, y_eos):
+	def __init__(self, num_encoder_layers, num_encoder_hidden, num_decoder_layers, num_decoder_hidden, Sx_size, Sy_size, y_eos, dropout):
 		super(EncoderDecoder, self).__init__()
-		self.encoder_rnn = 1
-		self.encoder_linear = 1
-		self.decoder_rnn = 1
-		self.decoder_linear = 1
-		self.decoder_log_softmax = 1
+		self.encoder_rnn = torch.nn.GRU(input_size=Sx_size, hidden_size=num_encoder_hidden, num_layers=num_encoder_layers, batch_first=True, dropout=dropout, bidirectional=True) #EncoderRNN(num_encoder_layers, num_encoder_hidden)
+		self.encoder_linear = torch.nn.Linear(num_encoder_hidden*2, num_decoder_hidden)
+		self.decoder_rnn = torch.nn.GRUCell(input_size=Sy_size, hidden_size=num_decoder_hidden) #DecoderRNN(num_decoder_layers, num_decoder_hidden)
+		self.decoder_linear = torch.nn.Linear(num_decoder_hidden, Sy_size)
+		self.decoder_log_softmax = torch.nn.LogSoftmax(dim=1)
 		self.y_eos = y_eos # index of the end-of-sequence token
-		self.Sx_size = Sx_size
-		self.Sy_size = Sy_size
 
 	def forward(x, y):
 		"""
@@ -22,10 +20,14 @@ class EncoderDecoder(torch.nn.Module):
 		"""
 
 		batch_size = y.shape[0]
+		Sy_size = y.shape[1]
 		U = y.shape[2]
 
 		# Encode the input sequence into a single fixed-length vector
-		encoder_state = self.encoder_rnn(x)
+		_, encoder_state = self.encoder_rnn(x)
+		encoder_state = torch.cat([encoder_state[-1], encoder_state[-2]])
+		print(x.shape)
+		print(encoder_state.shape)
 
 		# Initialize the decoder state using the encoder state
 		decoder_state = self.encoder_linear(encoder_state)
@@ -35,15 +37,15 @@ class EncoderDecoder(torch.nn.Module):
 		for u in range(0, U):
 			if u == 0:
 				# Feed in 0
-				decoder_state = self.decoder_rnn(y[u-1], decoder_state)
+				decoder_state = self.decoder_rnn(torch.zeros(batch_size, Sy_size), decoder_state)
 			else:
 				# Feed in the previous element of y; update the decoder state
-				decoder_state = self.decoder_rnn(y[u-1], decoder_state)
+				decoder_state = self.decoder_rnn(y[:,:,u-1], decoder_state)
 
 			# Compute log p(y_u|y_1, y_2, ..., x) (the log probability of the next element)
 			out = self.decoder_linear(decoder_state)
 			out = self.decoder_log_softmax(out)
-			log_p_yu = out[:,y[u]] 
+			log_p_yu = (out * y[:,:,u]).sum() # y_u is one-hot; use dot-product to select the y_u'th output probability
 
 			# Add log p(y_u|...) to log p(y|x)
 			log_p_y_x += log_p_yu
