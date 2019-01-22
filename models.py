2bc9b437be9a09075a46aceb89d22e198b403a76
loren.lugosch <loren.lugosch@gmail.com>
2019-01-22 16:17:49 -0500

    changes

--- models.py
+++ models.py
@@ -150,9 +150,8 @@ class EncoderDecoder(torch.nn.Module):
 
 			# Find the top output
 			y_hat_u_1 -= y_hat_u_1 # set to zero
-			y_hat_u_1[decoder_out.max(dim=1)[1]] += 1
-			for idx in range(batch_size):
-				y_hat[idx].append(Sy.index(decoder_out.max(dim=0)[1].item()))
+			y_hat_u_1[torch.arange(batch_size), decoder_out.max(dim=1)[1]] = 1.
+			y_hat[idx].append(y_hat_u_1.copy())
 
 		return y_hat
 
