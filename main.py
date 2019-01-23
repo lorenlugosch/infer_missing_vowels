62ab10fe415048a8f5d7428d0e8037d96d5cdaf0
loren.lugosch <loren.lugosch@gmail.com>
2019-01-23 14:46:34 -0500

    changes

--- main.py
+++ main.py
@@ -3,28 +3,8 @@ from models import EncoderDecoder
 from data import get_datasets, PadAndOneHot
 from training import Trainer
 
-# To use a different training text file, just change this path.
-# Each line separated by '\n' will be used as one training example.
-# with open("war_and_peace.txt", "r") as f:
-# 	data = f.readlines()
-# data[-1] += '\n'
-
-# # split dataset
-# total_lines = len(data)
-# one_tenth = total_lines // 10
-
-# train_dataset = TextDataset(data[0:one_tenth * 8])
-# # train_data_loader = train_dataset.loader
-
-# valid_dataset = TextDataset(data[one_tenth * 8: one_tenth * 9])
-# # valid_data_loader = valid_dataset.loader
-
-# test_dataset = TextDataset(data[one_tenth * 9:])
-
 path = "war_and_peace.txt"
 train_dataset, valid_dataset, test_dataset = get_datasets(path)
-train_data_loader = train_dataset.loader
-valid_data_loader = valid_dataset.loader
 
 # initialize model
 model = EncoderDecoder(	num_encoder_layers=2,
@@ -37,7 +17,6 @@ model = EncoderDecoder(	num_encoder_layers=2,
 						dropout=0.5)
 if torch.cuda.is_available(): model = model.cuda()
 
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 trainer = Trainer(model, lr=0.001)
 
 num_epochs = 10
@@ -45,54 +24,10 @@ for epoch in range(num_epochs):
 	print("========= Epoch %d of %d =========" % (epoch, num_epochs))
 
 	# train
-	train_acc = 0
-	train_loss = 0
-	num_samples = 0
-	model.train()
-	for idx, batch in enumerate(train_data_loader):
-		x,y = batch
-		batch_size = len(x)
-		num_samples += batch_size
-		if torch.cuda.is_available(): # move to inside of model.forward()?
-				x = x.cuda()
-				y = y.cuda()
-		log_probs = model(x,y); U = y.shape[1]
-		loss = -log_probs.mean() / U
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
-		model.eval(); y_hat = model.infer(x, Sy); model.train()
-		train_loss += loss.cpu().data.numpy().item() * batch_size
-		# train_acc += edit_distance(y,y_hat) * batch_size
-
-		# print out the model's guess, to see how well it's learning
-		if idx % 20 == 0:
-			print("input: " + "".join([Sx[c] for c in x[0].max(dim=1)[1] if c != x_eos]))
-			print("truth: " + "".join([Sy[c] for c in y[0].max(dim=1)[1] if c != y_eos]))
-			print("guess: " + "".join([Sy[c] for c in y_hat[0].max(dim=1)[1] if c != y_eos]))
-			print("")
-	train_loss /= num_samples
-	train_acc /= num_samples
+	train_acc, train_loss = trainer.train(train_dataset)
 
 	# valid
-	valid_acc = 0
-	valid_loss = 0
-	num_samples = 0
-	model.eval()
-	for idx, batch in enumerate(valid_data_loader):
-		x,y = batch
-		batch_size = len(x)
-		num_samples += batch_size
-		if torch.cuda.is_available():
-				x = x.cuda()
-				y = y.cuda()
-		log_probs = model(x,y); U = y.shape[1]
-		loss = -log_probs.mean() / U
-		y_hat = model.infer(x, Sy)
-		valid_loss += loss.cpu().data.numpy().item() * batch_size
-		# valid_acc += edit_distance(y,y_hat) * batch_size
-	valid_loss /= num_samples
-	valid_acc /= num_samples
+	valid_acc, valid_loss = trainer.test(valid_dataset)
 
 	#############################
 	test_output = "Hello, world!\n"
--- training.py
+++ training.py
@@ -4,15 +4,56 @@ class Trainer:
 	def __init__(self, model, lr):
 		self.model = model
 		self.lr = lr
+		self.optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
 		
 	def train(self, dataset):
+		train_acc = 0
+		train_loss = 0
+		num_samples = 0
 		self.model.train()
-		train_acc = 0.
-		train_loss = 0.
+		for idx, batch in enumerate(dataset.loader):
+			x,y = batch
+			batch_size = len(x)
+			num_samples += batch_size
+			if torch.cuda.is_available(): # move to inside of model.forward()?
+					x = x.cuda()
+					y = y.cuda()
+			log_probs = self.model(x,y); U = y.shape[1]
+			loss = -log_probs.mean() / U
+			self.optimizer.zero_grad()
+			loss.backward()
+			self.optimizer.step()
+			self.model.eval(); y_hat = self.model.infer(x, dataset.Sy); self.model.train()
+			train_loss += loss.cpu().data.numpy().item() * batch_size
+			# train_acc += edit_distance(y,y_hat) * batch_size
+
+			# print out the model's guess, to see how well it's learning
+			if idx % 20 == 0:
+				print("input: " + "".join([dataset.Sx[c] for c in x[0].max(dim=1)[1] if c != x_eos]))
+				print("truth: " + "".join([dataset.Sy[c] for c in y[0].max(dim=1)[1] if c != y_eos]))
+				print("guess: " + "".join([dataset.Sy[c] for c in y_hat[0].max(dim=1)[1] if c != y_eos]))
+				print("")
+		train_loss /= num_samples
+		train_acc /= num_samples
 		return train_acc, train_loss
 
 	def test(self, dataset):
+		test_acc = 0
+		test_loss = 0
+		num_samples = 0
 		self.model.eval()
-		test_acc = 0.
-		test_loss = 0.
+		for idx, batch in enumerate(dataset.loader):
+			x,y = batch
+			batch_size = len(x)
+			num_samples += batch_size
+			if torch.cuda.is_available():
+					x = x.cuda()
+					y = y.cuda()
+			log_probs = model(x,y); U = y.shape[1]
+			loss = -log_probs.mean() / U
+			y_hat = self.model.infer(x, Sy)
+			test_loss += loss.cpu().data.numpy().item() * batch_size
+			# test_acc += edit_distance(y,y_hat) * batch_size
+		test_loss /= num_samples
+		test_acc /= num_samples
 		return test_acc, test_loss
