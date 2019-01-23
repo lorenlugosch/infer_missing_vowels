2c3ff133629a909a8e7ad8a21ef6f78c65583eb5
loren.lugosch <loren.lugosch@gmail.com>
2019-01-23 13:51:46 -0500

    changes

--- data.py
+++ data.py
@@ -7,16 +7,13 @@ class TextDataset(torch.utils.data.Dataset):
 	def __init__(self, lines):
 		self.lines = lines
 		
-		# get size of input and output alphabets
-		c = Counter(("".join(lines)))
-		Sy = list(c.keys()) # set of possible output letters
-		Sy_size = len(Sy) # 82, including EOS
-		Sx = [letter for letter in Sy if letter not in "AEIOUaeiou"] # remove vowels from set of possible input letters
-		Sx_size = len(Sx) # 72, including EOS
+		# get input and output alphabets
+		self.Sy = list(Counter(("".join(lines))).keys()) # set of possible output letters
+		self.Sx = [letter for letter in Sy if letter not in "AEIOUaeiou"] # remove vowels from set of possible input letters
 		EOS_token = '\n' # all sequences end with newline
-		x_eos = Sx.index(EOS_token)
-		y_eos = Sy.index(EOS_token)
-		self.pad_and_one_hot = PadAndOneHot(Sx, Sy, x_eos, y_eos) # function for generating a minibatch from strings
+		self.x_eos = Sx.index(EOS_token)
+		self.y_eos = Sy.index(EOS_token)
+		self.pad_and_one_hot = PadAndOneHot(self.Sx, self.Sy, self.x_eos, self.y_eos) # function for generating a minibatch from strings
 		self.loader = torch.utils.data.DataLoader(self, batch_size=32, num_workers=1, shuffle=True, collate_fn=self.pad_and_one_hot)
 
 	def __len__(self):
