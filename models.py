ed6af4d8444c5ffeaf5a1bb8e29bf7dcc0195f8f
loren.lugosch <loren.lugosch@gmail.com>
2019-02-06 14:20:41 -0500

    lsdkj

--- models.py
+++ models.py
@@ -240,7 +240,7 @@ class EncoderDecoder(torch.nn.Module):
 				for extension_index in range(B):
 					extension = torch.zeros(batch_size, Sy_size)
 					extension_score = top_B_extension_scores[extension_index] + beam_score
-					extension[torch.arange(batch_size), top_B_extensions[extension_index]] = 1.
+					extension[torch.arange(batch_size), top_B_extensions[:,extension_index]] = 1.
 					beam_extensions.append(extension.clone())
 					beam_extension_scores.append(extension_score.clone())
 					beam_pointers.append(torch.ones(batch_size).long() * b) # we need to remember which hypothesis this extension belongs to
